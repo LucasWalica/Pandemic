ff46bcfb11d8d6f06d4561737ed74544fe16fcbf
LucasWalica <lucasw58@educastur.es>
2024-10-23 18:10:17 +0200

    test

--- Pandemic18/src/app/app.routes.ts
+++ Pandemic18/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { GamePandemicComponent } from './components/game-pandemic/game-pandemic.component';
+import { LogInPandemicComponent } from './components/log-in-pandemic/log-in-pandemic.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:InicioComponent},
+    {path:'login/', component:LogInPandemicComponent},
+    {path:'game/', component:GamePandemicComponent},
+];
