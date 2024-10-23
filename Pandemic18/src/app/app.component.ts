684ac13d190807f724fdfdfc873791c20173bf2c
LucasWalica <lucasw58@educastur.es>
2024-10-23 18:55:55 +0200

    profile medio hecho y añadidas imagenes

--- Pandemic18/src/app/app.component.ts
+++ Pandemic18/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from "./components/profile/profile.component";
+import { LogInPandemicComponent } from "./components/log-in-pandemic/log-in-pandemic.component";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { RegisterInPandemicComponent } from './components/register-in-pandemic/register-in-pandemic.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileComponent, LogInPandemicComponent, InicioComponent, RegisterInPandemicComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
